93ca5a7c0a31b3a8c5bccc06feb14d0204468144
Casey <leptone55@gmail.com>
2022-12-06 15:10:42 -0800

    cleaning

--- index.js
+++ index.js
@@ -14,53 +14,23 @@ async function fetchNodeChildren(id) {
   const response = await fetch(URL.replace(':id', id));
   const jsonResponse = await response.json();
   const node = jsonResponse[0];
-  // console.log(node);
 
   const children = node.child_node_ids;
   return children;
 }
 
 async function traverseAndTrackIds(childIds) {
-  console.log('childIds', childIds);
-  console.log('childIds.length', childIds.length);
-  calls++;
-  // console.log('calls', calls);
-  //const childrenIds = await fetchNodeChildren();
-
   for (let i = 0; i < childIds.length; i++) {
     const childId = childIds[i];
-    console.log('2222', childId);
     if (ids[childId]) {
-      console.log('in if childId', childId);
       ids[childId] += 1;
     } else {
-      console.log('in else childId', childId);
       ids[childId] = 1;
       const childsChildren = await fetchNodeChildren(childId);
-      console.log('childsChildren', childsChildren);
       await traverseAndTrackIds(childsChildren);
     }
   }
-  // childrenIds.forEach(async (childId) => {
-  //   // already exists on ids
-  //   if (ids[childId]) {
-  //     ids[childId] += 1;
-  //   } else {
-  //     ids[childId] = 1;
-  //     // fetch all children of the child
-  //     const childsChildren = await fetchNodeChildren(childId);
-  //     childsChildren.forEach(({ child_node_ids }) =>{
-  //       console.log('child_node_ids', child_node_ids)
-  //       traverseAndTrackIds(child_node_ids)
-  //     );
-  //   }
-  // });
 }
-
-// fetch node children
-
-// add node children to ids dctionary
-// increment if already exists
 
 async function main() {
   const entryChildren = await fetchNodeChildren(ID);
@@ -68,10 +38,7 @@ async function main() {
   await traverseAndTrackIds(entryChildren);
 
   console.log(ids);
-
-  // console.log('entryChildren', entryChildren);
-  // console.log('hello');
+  return ids;
 }
 
 main();
-// console.log(fetchNodeChildren(ID));
